8f9ff0fd993791d9b11f3fa3093099a0b8499f1a
maberizk <michelle.aberizk@gmail.com>
2024-01-09 14:27:40 -0800

    desktop home done

--- src/components/Footers.jsx
+++ src/components/Footers.jsx
@@ -1,14 +1,24 @@
 import React from "react";
+import FacebookIcon from "/assets/icon-facebook.svg";
+import InstagramIcon from "/assets/icon-instagram.svg";
+import TwitterIcon from "/assets/icon-twitter.svg";
 
 const Footers = () => {
   return (
     <>
-      <div>
-        <h1 className="upperCase">Modern Art Gallery</h1>
-        <p>
+      <div className="footer">
+        <h1 className="upperCase logoFooter">
+          Modern <br></br> Art Gallery
+        </h1>
+        <p className="footerText smallBody">
           The Modern Art Gallery is free to all visitors and open seven days a
           week from 8am to 9pm. Find us at 99 King Street, Newport, USA.
         </p>
+        <div className="icons">
+          <img src={FacebookIcon} alt="facebook icon" />
+          <img src={InstagramIcon} alt="instagram icon" />
+          <img src={TwitterIcon} alt="twitter icon" />
+        </div>
       </div>
     </>
   );
