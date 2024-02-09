29158e662597ff6f9e6c575e5fa4da62773651e5
maberizk <michelle.aberizk@gmail.com>
2024-02-08 16:10:25 -0800

    updates on location page

--- src/Location.jsx
+++ src/Location.jsx
@@ -6,26 +6,30 @@ const Location = () => {
   return (
     <div>
       <Map />
-      <div>
-        <h1>Our Location</h1>
-      </div>
-      <div>
-        <h2>99 King Street</h2>
-        <div>
-          <p>Newport</p>
-          <p>RI 02840</p>
-          <p>United States of America</p>
+      <div className="locationPageText">
+        <div className="locationPageHeader">
+          <h3 className="orange">Our Location</h3>
+        </div>
+        <div className="locationPageDetails">
+          <div>
+            <h4 className="orange">99 King Street</h4>
+            <div>
+              <p>Newport</p>
+              <p>RI 02840</p>
+              <p>United States of America</p>
+            </div>
+          </div>
+          <div>
+            <p>
+              Our newly opened gallery is located near the Edward King House on
+              99 King Street, the Modern Art Gallery is free to all visitors and
+              open seven days a week from 8am to 9am
+            </p>
+          </div>
         </div>
       </div>
-      <div>
-        <p>
-          Our newly opened gallery is located near the Edward King House on 99
-          King Street, the Modern Art Gallery is free to all visitors and open
-          seven days a week from 8am to 9am
-        </p>
-      </div>
 
-      <Footers />
+      <Footers page="location" />
     </div>
   );
 };
--- src/components/Footers.jsx
+++ src/components/Footers.jsx
@@ -3,20 +3,35 @@ import FacebookIcon from "/assets/icon-facebook.svg";
 import InstagramIcon from "/assets/icon-instagram.svg";
 import TwitterIcon from "/assets/icon-twitter.svg";
 
-const Footers = () => {
+const Footers = ({ page }) => {
+  const footerClass =
+    page === "location" ? "footer footerLocationsPage" : "footer";
+
+  const footerLogo =
+    page === "location"
+      ? "upperCase logoFooter logoFooterLoc"
+      : "uppercase logoFooter";
+
+  const footerText =
+    page === "location"
+      ? "footerText smallBody logoFooterLoc"
+      : "footerText smallBody";
+
+  const footerIcons = page === "location" ? "icons logoFooterLoc" : "icons";
+
   return (
     <>
-      <div className="footer">
+      <div className={footerClass}>
         <div className="footerTextContainer">
-          <h1 className="upperCase logoFooter">
+          <h1 className={footerLogo}>
             Modern <br></br> Art Gallery
           </h1>
-          <p className="footerText smallBody">
+          <p className={footerText}>
             The Modern Art Gallery is free to all visitors and open seven days a
             week from 8am to 9pm. Find us at 99 King Street, Newport, USA.
           </p>
         </div>
-        <div className="icons">
+        <div className={footerIcons}>
           <img src={FacebookIcon} alt="facebook icon" />
           <img src={InstagramIcon} alt="instagram icon" />
           <img src={TwitterIcon} alt="twitter icon" />
